af886bf587ef8665d64888076e233c0063956f3b
Manmohan Singh <manmohansingh016337@gmail.com>
2024-10-29 00:08:54 +0530

    till ex 1.9 was easy

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+hits =0
+while hits<10:
+    hits+=1
+    height = height*(3/5)
+    height = round(height,4)
+    print(hits, height)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,17 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.00
+rate = 0.05
+payment = 2684.11
+extra = 1000.00
+month = 0
+total_paid = 0.00
+while principal > 0:
+    principal = principal*(1+rate/12)-payment
+    month +=1
+    total_paid += payment
+    if month <=12:
+        total_paid +=extra
+        principal -=extra
+print(total_paid, month)
