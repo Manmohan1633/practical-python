54e256e55a9b8cdbc18cbdcc4b967f483a2714a2
Manmohan1633 <manmohansingh016337@gmail.com>
2024-11-09 22:21:59 +0530

    done ex 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    with open(filename, 'rt') as f:
+        header = next(f)
+        Total = 0.00
+        for line in f:
+            line = line.split(',')
+            cost = int(line[1]) * float(line[2])
+            Total += cost
+    return Total
+    
+
+print(f'Total cost to buy all shares is ${portfolio_cost('Work/Data/portfolio.csv')}')
+        
+    
