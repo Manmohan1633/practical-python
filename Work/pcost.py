74e927cbc31f252eefa20ec8739d760625ff37f9
Manmohan1633 <manmohansingh016337@gmail.com>
2024-11-09 22:26:53 +0530

    made a function for portfolio

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 def portfolio_cost(filename):
+
     with open(filename, 'rt') as f:
         header = next(f)
         Total = 0.00
@@ -10,8 +11,6 @@ def portfolio_cost(filename):
             cost = int(line[1]) * float(line[2])
             Total += cost
     return Total
-    
-
 print(f'Total cost to buy all shares is ${portfolio_cost('Work/Data/portfolio.csv')}')
         
     
